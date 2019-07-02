f5f9111e535d3ce6d32399c165170eaf03415524
William <727356424@qq.com>
2019-07-02 14:31:53 +0800

    fix bug

--- app/src/main/java/com/k12platformapp/k12photopicker/MainActivity.java
+++ app/src/main/java/com/k12platformapp/k12photopicker/MainActivity.java
@@ -2,6 +2,10 @@ package com.k12platformapp.k12photopicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import cn.k12cloud.k12photopicker.PhotoPickerUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnOpen = findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PhotoPickerUtils.startPhotoPickerActivity(MainActivity.this,10,100);
+            }
+        });
     }
 }
